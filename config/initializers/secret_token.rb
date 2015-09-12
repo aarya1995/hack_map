50a4be1871618a727c5c6b78df7a78a33374696c
Abhas Arya <abhasarya@0587359742.wireless.umich.net>
2015-09-12 01:43:59 -0400

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackMap::Application.config.secret_key_base = 'db5b8350603ab35e0590caed7bd767af2af8844a71d2422367d6301fa06c598332bb7784877269b01321547059a1b9e740539888c0bc13134dd0c3af46fa8ae8'
