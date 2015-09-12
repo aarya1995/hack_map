6c160b6cd645c665121e39cfe9abf980de130e82
Abhas Arya <abhasarya@0587359742.wireless.umich.net>
2015-09-12 05:56:26 -0400

    complete 95% of form besides geotagging

--- app/controllers/hack_maps_controller.rb
+++ app/controllers/hack_maps_controller.rb
@@ -1,4 +1,9 @@
 class HackMapsController < ApplicationController
 	def main
+
+	end
+
+	def create
+		
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ HackMap::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root 'hack_maps#main'
+
+  resources :hack_maps, :except => [:show]
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
